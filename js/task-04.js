b580ee75eaf66492f361db1b71025706bba2f66d
Rodion Romanenko <rod.romanenko@gmail.com>
2021-06-14 20:56:27 +0400

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,28 @@
-const increment = function (i) {
-    for (let i = 0; i < increment.length; i++) {
-        let count = 0;
-        count += i;
+const spanRef = document.getElementById('value');
+console.log(spanRef);
+const counterValue = spanRef;
+counterValue.textContent = value;
+console.log(counterValue);
 
-        return count;
-    }
+const incrementBtn = document.querySelector('[data-action = "increment"]');
+const decrementBtn = document.querySelector('[data-action = "decrement"]');
+
+incrementBtn.addEventListener('click', increment => {
+    console.log('Раз-раз!')
+});
+
+function increment (event) {
+    return counterValue.value += 1;
 }
 
 
-const decrement = function (y) {
-    for (let y = 0; y < decrement.length; y--) {
-        let count = 0;
-        count -= y;
+// const decrement = function (y) {
+//     for (let y = 0; y < decrement.length; y--) {
+//         let count = 0;
+//         count -= y;
 
-        return count;
-    }
-}
+//         return count;
+//     }
+// }
 
 
