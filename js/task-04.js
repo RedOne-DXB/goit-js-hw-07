fbe0a223f5bca5c3c47797c06e711e086e6fe025
Rodion Romanenko <rod.romanenko@gmail.com>
2021-06-15 21:23:05 +0400

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,39 +1,8 @@
-// const counterValue = {
-//     value: 0,
-//     increment() {
-//         this.value += 1;
-//     },
-//     decrement() {
-//         this.value -= 1;
-//     },
-// };
-
-
-// const counter = document.getElementById('value');
-// console.log(counterValue);
-
-// const incrementBtn = document.querySelector('[data-action = "increment"]');
-// const decrementBtn = document.querySelector('[data-action = "decrement"]');
-
-// incrementBtn.addEventListener('click', e => {
-//     console.log('Просто проверка работы прослушивания события')
-//     counterValue.increment();
-//     counter.textContent = counterValue.value
-// });
-   
-// decrementBtn.addEventListener('click', e => {
-//     console.log('Просто проверка работы прослушивания события')
-//     counterValue.decrement();
-//     counter.textContent = counterValue.value
-//    });
-
 let getValue = document.getElementById('value');
-console.log(getValue);
+let counterValue = 0;
 
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const decrementBtn = document.querySelector('[data-action="decrement"]');
-
-let counterValue = 0;
 
 incrementBtn.addEventListener('click', () => {
     getValue.textContent = counterValue += 1;
