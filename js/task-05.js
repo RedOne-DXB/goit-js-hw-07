903785d2a53052c929eb6879ffd786110cbb8976
Rodion Romanenko <rod.romanenko@gmail.com>
2021-06-16 11:59:10 +0400

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,17 +1,11 @@
 const nameInput = document.getElementById('name-input');
-console.log(nameInput);
 let nameOutput = document.getElementById('name-output');
-console.log(nameOutput);
-
-const defaultVal = nameOutput.defaultValue;
-
 
 nameInput.addEventListener('input', event => {
-    if (nameOutput.value == 0) {
-        defaultVal; // {`незнакомец`} ??? 
-    } else {
-        nameOutput.textContent = event.target.value;
-    }
+            nameOutput.textContent = event.target.value;
+    if (nameOutput.textContent.length === 0) {
+       return nameOutput.textContent = 'незнакомец'
+    } 
 });
 
 
